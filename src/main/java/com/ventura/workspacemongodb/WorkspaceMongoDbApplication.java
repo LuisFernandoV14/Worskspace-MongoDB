a2f1adf14b048b60aa9a744e8eb23575b276ad1a
LuisFenandoV14 <venturaluisf7@outlook.com>
2025-11-05 12:23:59 -0300

    Starting new Project
    Started a new project using Spring Initialzr.
    I will Maven, Spring Boot and MongoDB in this Project.

---
+++ src/main/java/com/ventura/workspacemongodb/WorkspaceMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.ventura.workspacemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkspaceMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ventura/workspacemongodb/WorkspaceMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventura.workspacemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
